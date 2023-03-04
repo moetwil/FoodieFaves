fecd96250f7d63959e40f863a9e70f2c6ef3ea58
moetwil <luc.moetwil@gmail.com>
2023-03-04 20:34:32 +0100

    add pagination to the searchresults

--- client/src/components/home/SearchSection.vue
+++ client/src/components/home/SearchSection.vue
@@ -8,7 +8,7 @@
                 <input class="w-50" type="text" name="search" id="searchInput" placeholder="Zoek een restaurant of stad"
                     v-model="searchQuery" />
                 <div class="search-results">
-                    <div v-for="result in searchResults" :key="result.id" class="search-result">
+                    <div v-for="(result, index) in paginatedResults" :key="result.id" class="search-result">
                         <div class="result-name">{{ result.name }}</div>
                         <div class="result-location">{{ result.city }}</div>
                     </div>
@@ -16,11 +16,24 @@
                         <div class="result-name">Niets gevonden</div>
                         <div class="result-location">Probeer een andere zoekopdracht.</div>
                     </div>
+                    <div v-if="showPagination" class="pagination">
+                        <button v-if="currentPage > 1" @click="currentPage--" class="page-link">
+                            Vorige
+                        </button>
+                        <button v-for="page in pages" :key="page" @click="currentPage = page" class="page-link"
+                            :class="{ active: page === currentPage }">
+                            {{ page }}
+                        </button>
+                        <button v-if="currentPage < totalPages" @click="currentPage++" class="page-link">
+                            Volgende
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
 <script setup lang="ts">
 import { ref, watch, computed } from 'vue';
 import Restaurant from "./../../interfaces/Restaurant";
@@ -29,6 +42,8 @@ const searchQuery = ref('');
 const searchResults = ref<Restaurant[]>([]);
 const noResultsMessage = "Niets gevonden";
 const noResults = { id: -1, name: noResultsMessage, city: '' };
+const perPage = 3;
+let currentPage = ref(1);
 
 watch(searchQuery, async (newQuery: string) => {
     const response = await fetch(`http://localhost/api/restaurants/search/${newQuery}`);
@@ -36,10 +51,28 @@ watch(searchQuery, async (newQuery: string) => {
     searchResults.value = results.length ? results : [];
 });
 
+const totalPages = computed(() => Math.ceil(searchResults.value.length / perPage));
+const pages = computed(() => {
+    const pagesArray = [];
+    for (let i = 1; i <= totalPages.value; i++) {
+        pagesArray.push(i);
+    }
+    return pagesArray;
+});
 
-const showNoResults = computed(() => searchResults.value.length === 0 && searchQuery.value !== '');
+const paginatedResults = computed(() => {
+    const startIndex = (currentPage.value - 1) * perPage;
+    const endIndex = startIndex + perPage;
+    return searchResults.value.slice(startIndex, endIndex);
+});
 
+const showNoResults = computed(() => {
+    return searchResults.value.length === 0 && searchQuery.value !== '';
+});
 
+const showPagination = computed(() => {
+    return searchResults.value.length > perPage;
+});
 </script>
 <style scoped>
 #searchInput {
@@ -89,5 +122,23 @@ const showNoResults = computed(() => searchResults.value.length === 0 && searchQ
 .result-location {
     color: #666;
     font-size: 14px;
+}
+
+/* Pagination */
+.pagination {
+    display: flex;
+    justify-content: center;
+}
+
+.page-link {
+    border: none;
+    background-color: transparent;
+    color: #333;
+    cursor: pointer;
+}
+
+.page-link:hover {
+    background-color: #f1f1f1;
+    border-radius: 5px;
 }
 </style>
