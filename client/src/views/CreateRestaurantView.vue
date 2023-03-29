e7f1e1a62163a1d9f520fc554b3bd5420fdea1ea
moetwil <luc.moetwil@gmail.com>
2023-03-29 13:14:02 +0200

    create restaurant refactor

--- client/src/views/CreateRestaurantView.vue
+++ client/src/views/CreateRestaurantView.vue
@@ -10,8 +10,8 @@
                         <div class="row">
                             <div class="field">
                                 <label class="label" for="name">Naam</label>
-                                <input v-model="name" class="input has-background-dark" type="text" name="name" id="name"
-                                    tabindex="1">
+                                <input v-model="restaurant.name" class="input has-background-dark" type="text" name="name"
+                                    id="name" tabindex="1">
                                 <div class="d-flex">
                                     <span class="icon px-2"><i></i></span>
                                     <p class="help"></p>
@@ -21,8 +21,8 @@
                         <div class="row">
                             <div class="field">
                                 <label class="label" for="phone">Telefoon</label>
-                                <input v-model="phone" class="input has-background-dark" type="text" name="phone" id="phone"
-                                    tabindex="1">
+                                <input v-model="restaurant.phone_number" class="input has-background-dark" type="text"
+                                    name="phone" id="phone" tabindex="1">
                                 <div class="d-flex">
                                     <span class="icon px-2"><i></i></span>
                                     <p class="help"></p>
@@ -33,8 +33,11 @@
                             <div class="col">
                                 <div class="field">
                                     <label class="label" for="category">Categorie</label>
-                                    <select class="form-select" v-model="category">
-                                        <option selected value="1">Kies een categorie</option>
+                                    <select class="form-select" v-model="restaurant.restaurant_type_id">
+                                        <option selected value="0">Kies een restaurant type</option>
+                                        <option v-for="restaurantType in restaurantTypes" :key="restaurantType.id"
+                                            :value="restaurantType.id">{{ restaurantType.name }}
+                                        </option>
                                     </select>
                                     <div class="d-flex">
                                         <span class="icon px-2"><i></i></span>
@@ -56,8 +59,8 @@
                             <div class="col-md-8">
                                 <div class="field">
                                     <label class="label" for="street">Straat</label>
-                                    <input v-model="street" class="input has-background-dark" type="text" name="street"
-                                        id="street" tabindex="1">
+                                    <input v-model="restaurant.street" class="input has-background-dark" type="text"
+                                        name="street" id="street" tabindex="1">
                                     <div class="d-flex">
                                         <span class="icon px-2"><i></i></span>
                                         <p class="help"></p>
@@ -67,7 +70,7 @@
                             <div class="col">
                                 <div class="field">
                                     <label class="label" for="street">Huisnummer</label>
-                                    <input v-model="houseNumber" class="input has-background-dark" type="text"
+                                    <input v-model="restaurant.house_number" class="input has-background-dark" type="text"
                                         name="house-number" id="house-number" tabindex="1">
                                     <div class="d-flex">
                                         <span class="icon px-2"><i></i></span>
@@ -80,8 +83,8 @@
                             <div class="col">
                                 <div class="field">
                                     <label class="label" for="street">Postcode</label>
-                                    <input v-model="zipCode" class="input has-background-dark" type="text" name="zip-code"
-                                        id="zip-code" tabindex="1">
+                                    <input v-model="restaurant.zip_code" class="input has-background-dark" type="text"
+                                        name="zip-code" id="zip-code" tabindex="1">
                                     <div class="d-flex">
                                         <span class="icon px-2"><i></i></span>
                                         <p class="help"></p>
@@ -91,8 +94,8 @@
                             <div class="col-md-8">
                                 <div class="field">
                                     <label class="label" for="street">Stad</label>
-                                    <input v-model="city" class="input has-background-dark" type="text" name="city"
-                                        id="city" tabindex="1">
+                                    <input v-model="restaurant.city" class="input has-background-dark" type="text"
+                                        name="city" id="city" tabindex="1">
                                     <div class="d-flex">
                                         <span class="icon px-2"><i></i></span>
                                         <p class="help"></p>
@@ -104,8 +107,8 @@
                             <div class="col">
                                 <div class="field">
                                     <label class="label" for="street">Land</label>
-                                    <input v-model="country" class="input has-background-dark" type="text" name="country"
-                                        id="country" tabindex="1">
+                                    <input v-model="restaurant.country" class="input has-background-dark" type="text"
+                                        name="country" id="country" tabindex="1">
                                     <div class="d-flex">
                                         <span class="icon px-2"><i></i></span>
                                         <p class="help"></p>
@@ -125,30 +128,37 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { useAuthenticationStore } from '../stores/authenticationStore';
-import Banner from '../components/Banner.vue';
-import ImageUpload from '../components/ImageUpload.vue';
-import axios from './../utils/axios';
 import {
     FieldMessageType,
     setFieldMessage, clearFieldMessages, hasAnyFieldErrors,
 } from "../utils/formUtils.js";
 import Restaurant from '../interfaces/Restaurant';
+import RestaurantType from '../interfaces/RestaurantType';
+import Banner from '../components/Banner.vue';
+import ImageUpload from '../components/ImageUpload.vue';
+import axios from './../utils/axios';
 const router = useRouter();
-const authenticationStore = useAuthenticationStore();
 
 // VARIABLES
-const name = ref('');
-const phone = ref('');
-const street = ref('');
-const houseNumber = ref('');
-const zipCode = ref('');
-const city = ref('');
-const country = ref('');
-const category = ref(0) as any;
-const imageFile = ref('');
+const restaurantTypes = ref<RestaurantType[]>([]);
+const restaurant = ref<Restaurant>({
+    name: '',
+    phone_number: '',
+    street: '',
+    house_number: '',
+    zip_code: '',
+    city: '',
+    country: '',
+    restaurant_type_id: 0,
+    profile_picture: '',
+});
+
+onMounted(async () => {
+    await fetchRestaurantTypes();
+});
 
 async function handleSubmit() {
     // CHECK FOR ERRORS
@@ -156,30 +166,26 @@ async function handleSubmit() {
         return;
     }
 
-    const newRestaurant: Restaurant = {
-        id: null,
-        name: name.value,
-        phone_number: phone.value,
-        owner_id: null,
-        street: street.value,
-        house_number: houseNumber.value,
-        zip_code: zipCode.value,
-        city: city.value,
-        country: country.value,
-        profile_picture: imageFile.value,
-        restaurant_type_id: category.value,
-    }
-
-    const response = await axios.post('/restaurants', newRestaurant);
-    if (response.status === 200) {
-        // router.push('/mijn-restaurants');
-        console.log(response);
-    }
+    // post restaurant
+    const response = await axios.post('/restaurants', restaurant.value);
+    router.push('/mijn-restaurants');
 
 }
 
 function handleImageUpload(image: string) {
-    imageFile.value = image;
+    restaurant.value!.profile_picture = image;
+}
+
+async function fetchRestaurantTypes() {
+    try {
+        const response = await axios.get('/restaurant-types');
+        if (response.status === 200) {
+            restaurantTypes.value = response.data;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 function checkForErrors() {
@@ -194,22 +200,22 @@ function checkForErrors() {
     // ERROR HANDLING
     clearFieldMessages();
 
-    if (!name.value)
+    if (!restaurant.value?.name)
         setFieldMessage(nameEl, FieldMessageType.Error, "Vul alstublieft een naam in.");
 
-    if (!phone.value)
+    if (!restaurant.value?.phone_number)
         setFieldMessage(phoneEl, FieldMessageType.Error, "Vul alstublieft een telefoonnummer in.");
 
-    if (!street.value)
+    if (!restaurant.value?.street)
         setFieldMessage(streetEl, FieldMessageType.Error, "Vul alstublieft een straat in.");
 
-    if (!houseNumber.value)
+    if (!restaurant.value?.house_number)
         setFieldMessage(houseNumberEl, FieldMessageType.Error, "Vul alstublieft een huisnummer in.");
 
-    if (!zipCode.value)
+    if (!restaurant.value?.zip_code)
         setFieldMessage(zipCodeEl, FieldMessageType.Error, "Vul alstublieft een postcode in.");
 
-    if (!city.value)
+    if (!restaurant.value?.city)
         setFieldMessage(cityEl, FieldMessageType.Error, "Vul alstublieft een stad in.");
 
     return hasAnyFieldErrors();
