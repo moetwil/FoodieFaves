ae47d91764fd1bf4a474f5b22f46b88a32ba5dea
moetwil <luc.moetwil@gmail.com>
2023-03-31 11:12:31 +0200

    restaurant controller beveiligen en refactor

--- README.md
+++ README.md
@@ -4,32 +4,15 @@
 
 General
 [] - Alles naar Nederlands
-[] - Reviews alleen tonen als ze approved zijn
 
 Client
 [] - Restaurants page: filters
 [] - Restaurants page: pagination stylen
 [] - Restaurant store: ombouwen met getters
-
-[] - Login: Client message error handling
-[x] - Register: Type account select
-
-[] - Admin: admin page maken
-[] - Admin: delete review
-[] - Admin: approve review
-[] - Admin: admin page in menu
-
-[] - My restaurants: categorieen uit de database
-
-
 [] - Review page: Textarea max aantal karakters
 [] - Review page: click on same amount = 0
-
 [] - My account: password alleen updaten als hij veranderd
 [] - My account: change role?
-
-[] - Home: Recente reviews de 2 laatste
-
 [] - Searchbar: Als input leeg is de preview weghalen
 
 Server
@@ -37,6 +20,7 @@ Server
 [] - Controller: Secret-key to .env
 [] - password weghalen bij users
 [] - UserRepository: password alleen updaten als hij veranderd
-[] - Check of username en email al taken is
+[] - Register/edit: Check of username en email al taken is
+[] - GetAllRestaurants: filter/order/pagination
 
 
--- server/app/controllers/restaurantcontroller.php
+++ server/app/controllers/restaurantcontroller.php
@@ -3,24 +3,26 @@
 namespace Controllers;
 
 use Exception;
-use Services\RestaurantService;
+use Services\{RestaurantService, UserService};
 
 class RestaurantController extends Controller
 {
     private $service;
+    private $userService;
 
     // initialize services
     function __construct()
     {
         $this->service = new RestaurantService();
+        $this->userService = new UserService();
     }
 
     public function getById($id)
     {
         try {
+            // get restaurant by id from the database and check if it is found
             $restaurant = $this->service->getRestaurantById($id);
-
-            if ($restaurant == null) {
+            if (!$restaurant) {
                 $this->respondWithError(404, "Restaurant not found");
                 return;
             }
@@ -35,9 +37,9 @@ class RestaurantController extends Controller
     public function getAll()
     {
         try {
+            // get all restaurants and check if any are found
             $restaurants = $this->service->getAllRestaurants();
-
-            if ($restaurants == null) {
+            if (!$restaurants) {
                 $this->respondWithError(404, "No restaurants found");
                 return;
             }
@@ -51,18 +53,15 @@ class RestaurantController extends Controller
 
     public function getAllByOwner($id)
     {
-        // $this->respond($id);
         try {
-            $newId = (int)$id;
-            $restaurants = $this->service->getAllRestaurantsByOwner($newId);
-
+            // get restaurants by owner id and check if any are found
+            $restaurants = $this->service->getAllRestaurantsByOwner($id);
             if ($restaurants == null) {
                 $this->respondWithError(404, "No restaurants found");
                 return;
             }
 
             $this->respond($restaurants);
-
         } catch (Exception $e) {
             $this->respondWithError(500, $e->getMessage());
         }
@@ -71,9 +70,21 @@ class RestaurantController extends Controller
     public function create()
     {
         try {
+            // verify jwt
             $decoded = $this->checkForJwt();
-            
+            if(!$decoded){
+                $this->respondWithError(401, "You are not authorized");
+                return;
+            }
+
+            // check if the user is of type owner
             $userId = $decoded->data->id;
+            $user = $this->userService->getUserById($userId);
+            if($user->user_type != 1){
+                $this->respondWithError(401, "You are not authorized to create a restaurant");
+                return;
+            }
+            
             $postedRestaurant = $this->createObjectFromPostedJson("Models\\Restaurant");
             $restaurant = $this->service->addRestaurant($postedRestaurant, $userId);
         } catch (Exception $e) {
@@ -85,22 +96,8 @@ class RestaurantController extends Controller
     public function update($id)
     {
         try {
-            // check for jwt
-            $decoded = $this->checkForJwt();
-
-            // get restaurant
-            $restaurant = $this->service->getRestaurantById($id);
-            if($restaurant == null) {
-                $this->respondWithError(404, "Restaurant not found");
-                return;
-            }
-            
             // check if user is owner of restaurant
-            $owner = $decoded -> data -> id;
-            if($restaurant->owner_id != $owner) {
-                $this->respondWithError(401, "You are not authorized to update this restaurant");
-                return;
-            }
+            if(!$this->authorizeOwnerAction($id))return;
 
             // update restaurant
             $postedRestaurant = $this->createObjectFromPostedJson("Models\\Restaurant");
@@ -114,70 +111,56 @@ class RestaurantController extends Controller
     public function delete($id)
     {
         try {
+            // verify jwt and check if the user is the owner of the restaurant
+            if(!$this->authorizeOwnerAction($id))return;
 
-            $decoded = $this->checkForJwt();
-
-            $restaurant = $this->service->getRestaurantById($id);
-
-            if($restaurant == null) {
-                $this->respondWithError(404, "Restaurant not found");
-                return;
-            }
-
-            $ownerId = $restaurant->owner_id;
-
-            // check if user is owner of restaurant
-            if($restaurant->owner_id != $decoded->data->id) {
-                $this->respondWithError(401, "You are not authorized to delete this restaurant");
-                return;
-            }
-
+            // delete restaurant
             $response = $this->service->deleteRestaurant($id);
-
-            if($response == false) {
+            if(!$response) {
                 $this->respondWithError(500, "Restaurant could not be deleted");
                 return;
             }
+            $this->respond($id);
         } catch (Exception $e) {
             $this->respondWithError(500, $e->getMessage());
         }
-
-        // $this->respond($response);
     }
 
     public function getRestaurantReviewsAmount($id)
     {
         try {
+            // get restaurant by id from the database and check if it is found
             $restaurant = $this->service->getRestaurantById($id);
-
             if($restaurant == null) {
                 $this->respondWithError(404, "Restaurant not found");
                 return;
             }
 
+            // get amount of reviews
             $amount = $this->service->getRestaurantReviewsAmount($id);
+            $this->respond($amount);
+
         } catch (Exception $e) {
             $this->respondWithError(500, $e->getMessage());
         }
-
-        $this->respond($amount);
     }
 
     public function getRestaurantRating($id){
         try {
+            // get restaurant by id from the database and check if it is found
             $restaurant = $this->service->getRestaurantById($id);
-
             if($restaurant == null) {
                 $this->respondWithError(404, "Restaurant not found");
                 return;
             }
 
+            // get rating
             $rating = $this->service->getRestaurantRating($id);
+            $this->respond($rating);
+
         } catch (Exception $e) {
             $this->respondWithError(500, $e->getMessage());
         }
-
-        $this->respond($rating);
     }
 
     public function search($query){
@@ -186,8 +169,38 @@ class RestaurantController extends Controller
         } catch (Exception $e) {
             $this->respondWithError(500, $e->getMessage());
         }
-
         $this->respond($restaurants);
+    }
+
+    private function authorizeOwnerAction($restaurantId){
+        // authorize
+        $decoded = $this->checkForJwt();
+        if(!$decoded) {
+            $this->respondWithError(401, "Not authorized");
+            return false;
+        }
+        
+        // check if the user is of type owner
+        $userId = $decoded->data->id;
+        $user = $this->userService->getUserById($userId);
+        if($user->user_type != 1){
+            $this->respondWithError(401, "You are not a restaurant owner");
+            return false;
+        }
+        
+        // get the restaurant and check if the user is the owner
+        $restaurant = $this->service->getRestaurantById($restaurantId);
+        if($restaurant == null) {
+            $this->respondWithError(404, "Restaurant not found");
+            return false;
+        }
+
+        if($restaurant->owner_id != $userId) {
+            $this->respondWithError(401, "You are not the owner of this restaurant");
+            return false;
+        }
+        
+        return true;
     }
 
 
